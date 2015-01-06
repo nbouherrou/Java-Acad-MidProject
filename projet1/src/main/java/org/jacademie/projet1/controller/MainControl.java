1ef11fe275dc9bfcfec94297a0c5771d2364482f
nbouherrou <nacerbouherrou@gmail.com>
2015-01-06 16:15:00 +0100

    controller

---
+++ projet1/src/main/java/org/jacademie/projet1/controller/MainControl.java
@@ -0,0 +1,16 @@
+package org.jacademie.projet1.controller;
+
+import java.nio.file.Path;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class MainControl {
+
+	private static Logger logger = LogManager.getLogger(MainControl.class);
+
+	public void mainControl(Path path) {
+
+	}
+
+}
